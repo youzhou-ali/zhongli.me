3e7f7fee87752c43b72bdda60794dcc4aa1d883f
li zhong <55781835+youzhou-ali@users.noreply.github.com>
2026-08-19 11:50:35 +0800

    Keep GitHub avatar on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,8 @@ import { posts } from "@/lib/blog";
 import { restorePostListAnchorForPath } from "@/lib/postTransition";
 import { useEffect } from "react";
 
+const avatarUrl = "https://avatars.githubusercontent.com/u/55781835?v=4";
+
 export default function Home() {
   useEffect(() => { restorePostListAnchorForPath("/"); }, []);
 
@@ -13,7 +15,14 @@ export default function Home() {
     <PageLayout>
       <section id="hero" className="home-intro intro-appear">
         <Link href="/about" className="portrait-wrap" aria-label="关于 zhongli">
-          <img src="/avatar.svg" alt="钟笠 / zhongli" width="160" height="160" fetchPriority="high" />
+          <img
+            src={avatarUrl}
+            alt="钟笠 / zhongli"
+            width="160"
+            height="160"
+            fetchPriority="high"
+            onError={(event) => { event.currentTarget.src = "/avatar.svg"; }}
+          />
         </Link>
         <div className="intro-copy">
           <h1>Hi, I'm <em>@zhongli</em>.</h1>
